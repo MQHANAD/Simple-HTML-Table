00fa8a65bf2d292141c8428b71b751579a57c91e
Mohanad <s202024100@kfupm.edu.sa>
2025-02-26 22:09:03 +0300

    commiting

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Endpoint to read CSV file and send data
+app.get('/data', (req, res) => {
+    const csvFilePath = path.join(__dirname, 'Table_Input.csv');
+    fs.readFile(csvFilePath, 'utf8', (err, data) => {
+        if (err) {
+            return res.status(500).send('Error reading CSV file');
+        }
+        res.send(data);
+    });
+});
+
+// Serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
